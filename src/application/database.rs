39628788cb22fcf7790c0bb53396f55045506772
Michael Howard <michael.howard.h@gmail.com>
2024-01-13 12:13:18 +0200

    Database init changes

--- src/application/database.rs
+++ src/application/database.rs
@@ -1,6 +1,10 @@
-use sqlx::{Pool, Postgres};
-use sqlx::postgres::PgPoolOptions;
 use std::env;
+use std::str::FromStr;
+use std::time::Duration;
+
+use log::LevelFilter;
+use sqlx::{ConnectOptions, Pool, Postgres};
+use sqlx::postgres::{PgConnectOptions, PgPoolOptions};
 
 #[derive(Clone, Debug)]
 pub struct DatabaseConfig {
@@ -8,7 +12,9 @@ pub struct DatabaseConfig {
     pub host: String,
     pub user: String,
     pub password: String,
-    pub database_name: String
+    pub database_name: String,
+    pub min_connections: u32,
+    pub max_connections: u32,
 }
 
 impl DatabaseConfig {
@@ -18,12 +24,28 @@ impl DatabaseConfig {
         let user = env::var("POSTGRES_USER").expect("Please define an ENV var for POSTGRES_USER");
         let password = env::var("POSTGRES_PASSWORD").expect("Please define an ENV var for POSTGRES_PASSWORD");
         let database_name = env::var("POSTGRES_DATABASE_NAME").expect("Please define an ENV var for POSTGRES_DATABASE_NAME");
+        let max_connections = match env::var("POSTGRES_MAX_CONNECTIONS") {
+            Ok(conn) => conn.parse::<u32>().unwrap(),
+            Err(_) => {
+                info!("POSTGRES_MAX_CONNECTIONS not set. Defaulting to 4");
+                4
+            }
+        };
+        let min_connections = match env::var("POSTGRES_MIN_CONNECTIONS") {
+            Ok(conn) => conn.parse::<u32>().unwrap(),
+            Err(_) => {
+                info!("POSTGRES_MIN_CONNECTIONS not set. Defaulting to 1");
+                1
+            }
+        };
         DatabaseConfig {
             port,
             host,
             user,
             password,
-            database_name
+            database_name,
+            min_connections,
+            max_connections,
         }
     }
 }
@@ -36,9 +58,22 @@ pub struct Database {
 impl Database {
     pub async fn init(config: DatabaseConfig) -> Database {
         let database_url = format!("postgres://{}:{}@{}:{}/{}", config.user, config.password, config.host, config.port, config.database_name);
+        let options = PgConnectOptions::from_str(&database_url).unwrap()
+            .disable_statement_logging()
+            .log_slow_statements(LevelFilter::Warn, Duration::from_millis(500))
+            .clone();
         let pool = match PgPoolOptions::new()
-            .max_connections(10)
-            .connect(&database_url)
+            .max_connections(config.max_connections)
+            .min_connections(2)
+            .after_connect(|_conn, _meta| Box::pin(async move {
+                debug!("Acquired new database connection");
+                Ok(())
+            }))
+            .after_release(|_conn, meta| Box::pin(async move {
+                debug!("Released database connection. Age: {} seconds, Idled: {} seconds", meta.age.as_secs(), meta.idle_for.as_secs());
+                Ok(true)
+            }))
+            .connect_with(options)
             .await
         {
             Ok(pool) => {
